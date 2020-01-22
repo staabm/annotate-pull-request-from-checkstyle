d1cbd1aad612f26ebda0c3d84d43036d34031cd0
Markus Staab <markus.staab@redaxo.de>
2020-01-22 21:01:40 +0100

    initil checkin

---
+++ cs2pr.php
@@ -0,0 +1,40 @@
+<?php
+
+$version = '1.0-dev';
+
+if ($argc === 1) {
+    $handle = fopen(STDIN, 'r');
+    $xml = stream_get_contents($handle);
+} elseif ($argc === 2 && file_exists($argv[1])) {
+    $xml = file_get_contents($argv[1]);
+} else {
+    echo "cs2pr $version\n";
+    echo "Annotate a Github Pull Request based on a Checkstyle XML-report.\n";
+    echo "Usage: php ". $argv[0] ." <filename>\n";
+    exit(1);
+}
+
+$root = simplexml_load_string($xml);
+
+foreach($root as $file) {
+    $filename = (string)$file['name'];
+
+    foreach($file as $error) {
+        $type = (string) $error['severity'];
+        $line = (string) $error['line'];
+        $message = (string) $error['message'];
+
+        annotateCheck($type, $filename, $line, $message);
+    }
+    exit();
+}
+
+/**
+ * @param 'error'|'warning' $type
+ * @param string $filename
+ * @param int $line
+ * @param string $message
+ */
+function annotateCheck($type, $filename, $line, $message) {
+    echo "::{$type} file={$filename},line={$line}::{$message}\n";
+}
